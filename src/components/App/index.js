6b8ced8a771d3daf3d31ab748c4b8f914bd30b88
SebCodeWeb <sebcodeweb@gmail.com>
2022-02-06 16:49:53 +0100

    clean code

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,16 +1,10 @@
 import React from 'react';
 
 // import reactLogo from './react-logo.svg'; <img src={reactLogo} alt="react logo" />
-import './app.scss';
-import Header from 'src/components/Header';
-import Main from 'src/components/Main';
-import Footer from 'src/components/Footer';
 
 const App = () => (
   <div className="app">
-    <Header />
-    <Main />
-    <Footer />
+   <h1>Test</h1>
   </div>
 );
 
